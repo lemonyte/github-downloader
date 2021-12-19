d527469a1b14727c53cd9702e13ef14d720e7509
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2021-12-19 13:33:35 -0500

    Commit changes

--- github-downloader.py
+++ github-downloader.py
@@ -1,42 +1,84 @@
+import os
+import argparse
 import requests
 
 
-def get_repos(owner: str):
-    if 'users/' not in owner and 'orgs/' not in owner:
-        owner = 'users/' + owner
-    url = f'https://api.github.com/{owner}/repos'
-    response = requests.get(url, allow_redirects=True)
-    repos = [r['name'] for r in response.json()]
-    return repos
+def get_repos(user: str, token: str = '') -> dict:
+    if 'users/' not in user and 'orgs/' not in user:
+        user = 'users/' + user
+    return requests.get(f'https://api.github.com/{user}/repos', allow_redirects=True, auth=(user, token)).json()
 
 
-def get_real_name(owner: str, repo: str):
-    repos = get_repos(owner)
-    for r in repos:
-        if r.lower() == repo:
-            return r
-    return repo
+def get_repo_json(user: str, repo_name: str, token: str = '') -> dict:
+    repos = get_repos(user, token)
+    for repo in repos:
+        if repo['name'].casefold() == repo_name.casefold():
+            return repo
+    raise Exception(f"Repository '{repo_name}' not found")
 
 
-def download_zip(owner: str, repo: str, branch: str = 'main', dest: str = '.'):
-    url = f'https://github.com/{owner}/{repo}/archive/{branch}.zip'
-    with open(f'{dest}/{repo}-{branch}.zip', 'wb') as file:
-        file.write(requests.get(url, allow_redirects=True).content)
+def download_zip(user: str, repo: dict, branch: str = '', dest: str = '.', token: str = ''):
+    branch = branch if branch else repo['default_branch']
+    url = f"https://github.com/{user}/{repo['name']}/archive/{branch}.zip"
+    response = requests.get(url, allow_redirects=True, auth=(user, token))
+    content_type = response.headers['Content-Type']
+    if content_type == 'application/zip':
+        filename = response.headers['content-disposition'].split('=')[1]
+    else:
+        filename = f"{repo['name']}-{branch}.txt"
+    with open(f'{dest}/{filename}', 'wb') as file:
+        file.write(response.content)
+
+
+def clone(user: str, repo: dict, dest: str = '.', token: str = ''):
+    cwd = os.getcwd()
+    os.chdir(dest)
+    if token == '':
+        os.system(f"git clone https://github.com/{user}/{repo['name']}.git")
+    else:
+        os.system(f"git clone https://{token}@github.com/{user}/{repo['name']}.git")
+    os.chdir(cwd)
 
 
 if __name__ == '__main__':
     try:
-        owner = str(input("Owner: "))
-        repo = str(input("Repository (type $all to download all repos): "))
-        branch = str(input("Branch: "))
-        branch = 'main' if branch == '' else branch
-        dest = str(input("Destination: "))
-        dest = '.' if dest == '' else dest
-
-        if repo == '$all':
-            for repo in get_repos(owner):
-                download_zip(owner, repo, branch, dest)
-        else:
-            download_zip(owner, get_real_name(owner, repo), branch, dest)
+        parser = argparse.ArgumentParser(description="Download or clone GitHub repositories.")
+        parser.add_argument('-u', '--user', default='', help="GitHub username")
+        parser.add_argument('-r', '--repo', default='*', help="repository name, '*' for all repos, default is '*'")
+        parser.add_argument('-b', '--branch', default='', help="repository branch, default is the default repo branch")
+        parser.add_argument('-d', '--dest', default='.', help="destination directory, default is '.'")
+        parser.add_argument('-t', '--token', default='', help="GitHub personal access token for private repos, default is ''")
+        parser.add_argument('-o', '--operation', default='zip', choices=['zip', 'clone', 'list'], help="operation to execute, 'zip', 'clone', or 'list', default is 'zip'")
+        args = parser.parse_args()
+        user, repo, branch, dest, token, operation = vars(args).values()
+        if not user:
+            user = input("Username: ").strip()
+            repo = input("Repository (enter '*' to download all repos): ").strip()
+            repo = repo if repo else '*'
+            branch = input("Branch: ").strip()
+            dest = input("Destination: ").strip()
+            dest = dest if dest else '.'
+            token = input("Token: ").strip()
+            operation = input("Operation ('zip', 'clone', or 'list'): ").strip().lower()
+            if not user:
+                raise Exception("Please enter a valid GitHub username")
+            if operation not in ['zip', 'clone', 'list']:
+                raise Exception("Please enter a valid operation: 'zip', 'clone', or 'list'")
+        if operation == 'list':
+            print(f"Repositories for user '{user}':")
+            for repo in get_repos(user, token):
+                print(f"name: {repo['name']}, default branch: {repo['default_branch']}, private: {repo['private']}")
+        elif operation == 'zip':
+            if repo == '*':
+                for repo in get_repos(user, token):
+                    download_zip(user, repo, branch, dest, token)
+            else:
+                download_zip(user, get_repo_json(user, repo, token), branch, dest, token)
+        elif operation == 'clone':
+            if repo == '*':
+                for repo in get_repos(user, token):
+                    clone(user, repo, dest, token)
+            else:
+                clone(user, get_repo_json(user, repo, token), dest, token)
     except Exception as exception:
         print(f"Fatal error: {exception}")
--- readme.md
+++ readme.md
@@ -1,18 +1,20 @@
 # GitHub Downloader
 By [LemonPi314](https://github.com/LemonPi314)
 
-Short description.
+Download or clone GitHub repositories.
 ## Requirements
 ### Python File
 Any operating system with Python.
 - [Python 3.9](https://www.python.org/downloads/) or higher
-- [requests](https://pypi.org/project/requests/)
+- [`requests`](https://pypi.org/project/requests/)
 ### Windows Systems
 Optional executable file for Windows users. Python and the required packages are included in the executable.
-- 30 MB of free space for the executable
+- 7 MB of free space for the executable
 - 70 MB of free space for temporary files
 ## Usage
-Usage instructions.
+Download the source code or executable file for Windows users from the [latest release](https://github.com/LemonPi314/github-downloader/releases/latest)
+
+If you want to download private repositories, create a personal access token for GitHub [here](https://github.com/settings/tokens)
 ## Disclaimer
 I am not liable for any data loss, damage, or any other consequences resulting from use of this software. Use at your own risk.
 ## License
